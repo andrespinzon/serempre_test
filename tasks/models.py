21cfaea0b7fc655f6b3cc7bf760b071462375d9e
andrespinzon <inge.andrespinzon@gmail.com>
2020-10-01 12:34:41 -0500

    Feat | add model taskk

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db.models import AutoField, CharField, BooleanField, DateTimeField, Model, PositiveIntegerField, FloatField
+
+
+class TodoList(Model):
+    id: int = AutoField(primary_key=True)
+    developer: str = CharField('Developer', max_length=255, null=False)
+    title: str = CharField('Title', max_length=255, null=False)
+    description: str = CharField('Description', max_length=32, null=False)
+    time_worked: int = PositiveIntegerField('Time Worked')
+    estimated_time: float = FloatField('Estimated Time')
+    is_completed: bool = BooleanField('Is Completed', default=True, null=False)
+    created_at: datetime = DateTimeField('Created', auto_now_add=True, db_index=True)
+    updated_at: datetime = DateTimeField('Updated', auto_now=True)
